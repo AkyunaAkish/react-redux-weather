569d5386c469bbd515ee665c6ce21beda7986062
AkyunaAkish <akyunaakish@gmail.com>
2016-06-19 11:40:02 -0600

    updated readme and removed comments

---
+++ README.md
@@ -0,0 +1,19 @@
+# ReactJS redux weather application - Akyuna Akish
+
+Built with:
+```
+ReactJS
+Redux
+React-google-maps
+React-sparklines
+Open Weather Map API
+Axios
+WebpackJS
+JSX
+HTML
+CSS
+```
+
+- Live Site:
+
+[http://react-redux-weather.s3-website-us-west-2.amazonaws.com/]('http://react-redux-weather.s3-website-us-west-2.amazonaws.com/')
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,11 +3,6 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-    // using concat instead of push as
-    // to not mutate the original state
-    // but to return a new instance of state
-    // return state.concat([action.payload.data]);
-    // other approach with spread attribute
     return [action.payload.data, ...state]
   }
   return state;
